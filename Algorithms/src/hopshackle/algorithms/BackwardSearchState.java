35be73790ed7a431cbe2b0de22dc0b2c0b9baeb9
hopshackle <james@janigo.co.uk>
2015-06-04 11:51:21 +0100

    Removed unused complete variable

--- Algorithms/src/hopshackle/algorithms/BackwardSearchState.java
+++ Algorithms/src/hopshackle/algorithms/BackwardSearchState.java
@@ -8,11 +8,9 @@ public class BackwardSearchState implements State {
 
 	private PDDLProblemDomain domain;
 	private List<PDDLCondition> knownPredicates;
-	private boolean complete;	// indicates that predicate list is exhaustive; any positive ones not enumerated are false
-
+	
 	public BackwardSearchState(PDDLProblemDomain problemDomain, List<PDDLCondition> predicates, boolean complete) {
 		domain = problemDomain;
-		this.complete = complete;
 		knownPredicates = new ArrayList<PDDLCondition>();
 		for (PDDLCondition p : predicates)
 			knownPredicates.add(new PDDLCondition(p));
